af70da60e19a88abc1419830ace1965ab655518d
woonys <wodns1324@naver.com>
2022-10-12 23:26:51 +0900

    순환 참조 깨뜨리기

--- ObjectDependencyExample/src/main/java/com/example/objectdesign/evaluation/DateObjectEvaluationProcessor.java
+++ ObjectDependencyExample/src/main/java/com/example/objectdesign/evaluation/DateObjectEvaluationProcessor.java
@@ -6,15 +6,17 @@ import java.util.Optional;
 import com.example.objectdesign.domain.DateObject;
 
 public class DateObjectEvaluationProcessor {
-    private static final int OBJECT_ALIVE_DAYS = 30;
 
-    public static boolean isDateAlive(DateObject obj) {
-        return Optional.ofNullable(obj.getUpdatedAt())
-                       .map(updatedAt -> updatedAt.toLocalDateTime().plusDays(OBJECT_ALIVE_DAYS))
-                       .map(isDateAlive -> LocalDateTime.now().isAfter(isDateAlive))
-                       .orElse(false);
-    }
-
+/* -> domain으로 코드 이전
+//    private static final int OBJECT_ALIVE_DAYS = 30;
+//
+//    public static boolean isDateAlive(DateObject obj) {
+//        return Optional.ofNullable(obj.getUpdatedAt())
+//                       .map(updatedAt -> updatedAt.toLocalDateTime().plusDays(OBJECT_ALIVE_DAYS))
+//                       .map(isDateAlive -> LocalDateTime.now().isAfter(isDateAlive))
+//                       .orElse(false);
+//    }
+*/
     public void DateAliveProcess() {
         //...
     }
