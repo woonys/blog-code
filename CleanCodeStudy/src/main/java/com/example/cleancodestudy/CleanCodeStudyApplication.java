1e0981001093e209b9dfee1dbb415b1cbde436ce
woonys <wodns1324@naver.com>
2022-10-28 19:44:02 +0900

    Clean code Study: 2장

---
+++ CleanCodeStudy/src/main/java/com/example/cleancodestudy/CleanCodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleancodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeStudyApplication.class, args);
+	}
+
+}
---
+++ CleanCodeStudy/src/test/java/com/example/cleancodestudy/CleanCodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleancodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
