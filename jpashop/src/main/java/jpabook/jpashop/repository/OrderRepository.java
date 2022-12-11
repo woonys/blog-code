d493c1a7b0456daaec846a7d1f5c507682dc0676
woonys <wodns1324@naver.com>
2022-12-11 21:22:29 +0900

    서버 파트 - done

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,20 @@
 package jpabook.jpashop.repository;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +33,104 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    public List<Order> findAllByString(OrderSearch orderSearch) { // 동적쿼리를 JPQL 문자열로 생성하는 방식 -> 안티패턴
+        // 동적쿼리를 짜려면?
+        List<Order> resultList = em.createQuery("select o from Order o join o.member m" +
+                                                " where o.status = :status " +
+                                                " and m.name like :name", Order.class)
+                                   .setParameter("status", orderSearch.getOrderStatus())
+                                   .setParameter("name", orderSearch.getMemberName())
+                                   .setMaxResults(1000) // 최대 1000 건
+                                   .getResultList();// JPQL 작성
+
+        /* 동적 쿼리의 안 좋은 예시
+        - 문제 1: 이렇게 JPQL을 문자로 생성하면 번거로움
+        - 문제 2 : 어디서 버그가 나타나는지 찾기가 매우 힘들다... 반면 MyBatis: 동적 쿼리를 생성하기 매우 좋다는 이점이 있다?
+         */
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += "where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " and";
+        }
+        jpql += " m.name like :name";
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                                    .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    /**
+     * JPA criteria로 동적 쿼리 -> 역시 별로 좋은 방식은 아님..
+     * 방식: JPQL을 자바 코드로 작성할 수 있도록 JPA에서 제공하는 표준 방. 동적 쿼리를 작성할 때 큰 도움.
+     * 단점: 유지보수성이 제로에 가까움 -> 무슨 쿼리가 생성될지 감이 전혀 오지 않는다!
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus()); // predicate가 조건이 된다.
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
+    /**
+     * 해결책: QueryDSL! -> 동적 쿼리 해결 방안! -> 동적 쿼리 뿐만 아니라 복잡한 정적 쿼리 역시 QueryDSL로 짠다!
+     */
+
 //    public List<Order> findAll(OrderSearch orderSearch) {
+//        QOrder order = QOrder.order;
+//        QMmember member = QMember.member;
 //
+//        return query
+//            .select(order)
+//            .from(order)
+//            .join(order.member, member)
+//            .where(statusEq(orderSearch.getOrderStatus()),
+//                   nameLike(orderSearch.getMemberName()))
+//            .limit
+//            .fetch();
 //    }
 }
---
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,109 @@
+package jpabook.jpashop.service;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import jpabook.jpashop.repository.OrderRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), 2);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = createMember();
+        Book item = createBook("시골 JPA",10000,10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소시 상태는 CANCEL", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문 취소시 재고 복구", 10, item.getStockQuantity());
+    }
+
+    @Test(expected = NotEnoughStockException.class) // 재고 수량 초과하면 해당 exception 뜨는지 확인
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 11;
+        //when
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //then
+        fail("재고 수량 부족 예외가 발생해야 함"); // 만약 위에서 예외가 발생하지 않으면 여기까지 내려온다는 건데, 그러면 문제가 있다는 것이니 fail처리를 해주는 것
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    /**
+     * 도메인 패턴 모델 썼을 때 장점;
+     * 특정 엔티티에서 바로 테스트 작성하기 좋음 -> 엔티티 안에 비즈니스 로직이 있으니
+     * DB 묶지 않고 단위 테스트로 작성하는 게 좋다.
+     */
+}
