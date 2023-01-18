5200aaba1d78fae35c9003c7f73b2ffbd86b60ef
woonys <wodns1324@naver.com>
2023-01-18 22:11:53 +0900

    다양한 연관관계 매핑: 일대일

---
+++ hellojpa/src/main/java/hellojpa/teamexample/Locker.java
@@ -0,0 +1,19 @@
+package hellojpa.teamexample;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    private Member member;
+}
--- hellojpa/src/main/java/hellojpa/teamexample/Member.java
+++ hellojpa/src/main/java/hellojpa/teamexample/Member.java
@@ -1,11 +1,6 @@
 package hellojpa.teamexample;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 public class Member {
@@ -17,6 +12,10 @@ public class Member {
 //    @ManyToOne // Member 입장에서 N(Many)이고 Team 입장에서 1(One)이니!
 //    @JoinColumn(name = "TEAM_ID")
 //    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID") // JoinColumn은 반드시 있어야 됨!
+    private Locker locker;
 
     public Long getId() {
         return id;
