b5a8041bac0452f1e0b4237305eccebf5eb74fb8
woonys <wodns1324@naver.com>
2023-09-15 22:49:18 +0900

    스프링 DB 1 - 트랜잭션 예제1 완료

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -4,9 +4,11 @@ import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,9 +36,17 @@ class MemberServiceV1Test {
         memberService = new MemberServiceV1(memberRepository);
     }
 
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
     @Test
     @DisplayName("정상 이체")
     public void accountTransfer() throws SQLException {
+
         //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberB = new Member(MEMBER_B, 10000);
@@ -50,5 +60,25 @@ class MemberServiceV1Test {
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
         assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    public void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+        //when
+        assertThatThrownBy(() ->
+            memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000)
+        ).isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
     }
 }
