40f787ead111033744c1a30db21e14feed3e93e0
woonys <wodns1324@naver.com>
2023-11-02 00:37:09 +0900

    7가지 동시성 모델 - 2.2.1 스레드 만들기

---
+++ java-seven-concurrency-models/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread = new Thread(() -> System.out.println("Hello from new thread"));
+        myThread.start();
+        Thread.yield();
+        System.out.println("Hello from main thread");
+        myThread.join();
+    }
+}
