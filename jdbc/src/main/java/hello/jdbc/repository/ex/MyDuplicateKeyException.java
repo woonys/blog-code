d71e4212d70db7416619d717c16edd4e948144cc
woonys <wodns1324@naver.com>
2024-01-02 21:33:11 +0900

    스프링 DB 1 - 데이터 접근 예외 직접 만들기:

--- jdbc/src/main/java/hello/jdbc/repository/ex/MyDuplicateKeyException.java
+++ jdbc/src/main/java/hello/jdbc/repository/ex/MyDuplicateKeyException.java
@@ -1,2 +1,19 @@
-package hello.jdbc.repository.ex;public class MyDuplicateKeyException {
+package hello.jdbc.repository.ex;
+
+public class MyDuplicateKeyException extends MyDbException { // 이 예외는 데이터 중복인 경우에만 던진다.
+    // 이 예외는 우리가 직접 만들었기 때문에 특정 기술에 종속적이지 않다. 따라서 서비스 계층 순수성을 유지 가능.
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
 }
