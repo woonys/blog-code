68f783ac72d72b2ff3b4cf5599a5125eff9372ac
woonys <wodns1324@naver.com>
2024-08-30 23:46:37 +0900

    ch.6 is palindrome

---
+++ JavaAlgorithmStudy/src/main/java/org/example/chapter6/IsPalindrome.java
@@ -0,0 +1,61 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class Solution {
+    /**My solution
+     * Runtime: 22ms Beats 14.11%
+     * Memory: 45.64MB Beats 14.50%
+    **/
+    public boolean isPalindrome(String s) {
+        String pattern = "[A-Za-z0-9]";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(s);
+        StringBuilder sb = new StringBuilder();
+        while(m.find()) {
+            sb.append(m.group());
+        }
+
+        String result = sb.toString().toLowerCase();
+        if (result.length() == 0 || result.length() == 1) {
+            return true;
+        };
+        if (result.length() > 0 && result.length() % 2 == 0) {
+            return false;
+        }
+
+        StringBuilder palinBuilder = new StringBuilder();
+        for (int i = result.length()-1; i >= 0; i--) {
+            palinBuilder.append(result.charAt(i));
+        }
+        String palin = palinBuilder.toString();
+        System.out.println("palin: " + palin);
+        if (!result.equals(palin)) {
+            return false;
+        }
+        return true;
+    }
+
+    /** Book solution 1
+     * Runtime: 2ms Beats 99.18%
+     * Memory: 42.35MB Beats 99.24%
+     **/
+    public boolean isPalindrome2(String s) {
+        int start = 0;
+        int end = s.length()-1;
+        while (start < end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+            else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
